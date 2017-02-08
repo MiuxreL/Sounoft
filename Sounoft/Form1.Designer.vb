451b82e7ea6714e642f00334a4a27d17c76773f5
MiuxreL <MIUXREL@hotmail.com>
2017-02-08 23:56:33 +0100

    Build 2
    [*] TESTS

--- Sounoft/Form1.Designer.vb
+++ Sounoft/Form1.Designer.vb
@@ -22,17 +22,99 @@ Partial Class Form1
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MaterialRaisedButton1 = New MaterialSkin.Controls.MaterialRaisedButton()
+        Me.MaterialContextMenuStrip1 = New MaterialSkin.Controls.MaterialContextMenuStrip()
+        Me.SélectionnerUnNomToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ChangerDeNomToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripTextBox1 = New System.Windows.Forms.ToolStripTextBox()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.LireLaudioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MaterialContextMenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MaterialRaisedButton1
+        '
+        Me.MaterialRaisedButton1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MaterialRaisedButton1.ContextMenuStrip = Me.MaterialContextMenuStrip1
+        Me.MaterialRaisedButton1.Depth = 0
+        Me.MaterialRaisedButton1.Location = New System.Drawing.Point(12, 76)
+        Me.MaterialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialRaisedButton1.Name = "MaterialRaisedButton1"
+        Me.MaterialRaisedButton1.Primary = False
+        Me.MaterialRaisedButton1.Size = New System.Drawing.Size(308, 173)
+        Me.MaterialRaisedButton1.TabIndex = 0
+        Me.MaterialRaisedButton1.Text = "But_0"
+        Me.MaterialRaisedButton1.UseVisualStyleBackColor = True
+        '
+        'MaterialContextMenuStrip1
+        '
+        Me.MaterialContextMenuStrip1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.MaterialContextMenuStrip1.Depth = 0
+        Me.MaterialContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SélectionnerUnNomToolStripMenuItem, Me.ToolStripSeparator1, Me.ChangerDeNomToolStripMenuItem, Me.ToolStripSeparator2, Me.LireLaudioToolStripMenuItem})
+        Me.MaterialContextMenuStrip1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialContextMenuStrip1.Name = "MaterialContextMenuStrip1"
+        Me.MaterialContextMenuStrip1.Size = New System.Drawing.Size(193, 82)
+        '
+        'SélectionnerUnNomToolStripMenuItem
+        '
+        Me.SélectionnerUnNomToolStripMenuItem.Name = "SélectionnerUnNomToolStripMenuItem"
+        Me.SélectionnerUnNomToolStripMenuItem.Size = New System.Drawing.Size(192, 22)
+        Me.SélectionnerUnNomToolStripMenuItem.Text = "Sélectionner un fichier"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(189, 6)
+        '
+        'ChangerDeNomToolStripMenuItem
+        '
+        Me.ChangerDeNomToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripTextBox1})
+        Me.ChangerDeNomToolStripMenuItem.Name = "ChangerDeNomToolStripMenuItem"
+        Me.ChangerDeNomToolStripMenuItem.Size = New System.Drawing.Size(192, 22)
+        Me.ChangerDeNomToolStripMenuItem.Text = "Changer de nom"
+        '
+        'ToolStripTextBox1
+        '
+        Me.ToolStripTextBox1.Name = "ToolStripTextBox1"
+        Me.ToolStripTextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.ToolStripTextBox1.Text = "BUT_0"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(189, 6)
+        '
+        'LireLaudioToolStripMenuItem
+        '
+        Me.LireLaudioToolStripMenuItem.Name = "LireLaudioToolStripMenuItem"
+        Me.LireLaudioToolStripMenuItem.Size = New System.Drawing.Size(192, 22)
+        Me.LireLaudioToolStripMenuItem.Text = "Lire l'audio"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1152, 605)
+        Me.ClientSize = New System.Drawing.Size(332, 261)
+        Me.Controls.Add(Me.MaterialRaisedButton1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Sounoft"
+        Me.MaterialContextMenuStrip1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents MaterialRaisedButton1 As MaterialSkin.Controls.MaterialRaisedButton
+    Friend WithEvents MaterialContextMenuStrip1 As MaterialSkin.Controls.MaterialContextMenuStrip
+    Friend WithEvents SélectionnerUnNomToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ChangerDeNomToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripTextBox1 As ToolStripTextBox
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents LireLaudioToolStripMenuItem As ToolStripMenuItem
 End Class
--- Sounoft/Form1.vb
+++ Sounoft/Form1.vb
@@ -3,9 +3,39 @@
 Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Skin
         Dim SkinManager As MaterialSkinManager = MaterialSkinManager.Instance
         SkinManager.AddFormToManage(Me)
         SkinManager.Theme = MaterialSkinManager.Themes.DARK
-        SkinManager.ColorScheme = New ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE)
+        SkinManager.ColorScheme = New ColorScheme(Primary.Blue800, Primary.Blue900, Primary.Blue500, Accent.LightBlue200, TextShade.WHITE)
+
+        'INTO > dem
+        dem()
+    End Sub
+
+    Private Sub SélectionnerUnNomToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SélectionnerUnNomToolStripMenuItem.Click
+        Dim OSD As New OpenFileDialog
+        OSD.Filter = "Wav sound (.wav)|*.wav"
+        If OSD.ShowDialog = DialogResult.OK Then
+            Dim Change As String() = My.Settings.VarBut.Split("-SOUNOFT-")
+            My.Settings.VarBut = My.Settings.VarBut.Replace(Change(1), OSD.FileName)
+            MsgBox(My.Settings.VarBut)
+        End If
+    End Sub
+
+    Sub dem()
+        Dim Gene As String() = My.Settings.VarBut.Split("-SOUNOFT-")
+        MaterialRaisedButton1.Text = Gene(2)
+    End Sub
+
+    Private Sub MaterialRaisedButton1_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton1.Click
+        Try
+            Dim Loadm As String() = My.Settings.VarBut.Split("-SOUNOFT-")
+            My.Computer.Audio.Play("‪C:\Users\MiuxreL\Documents\MEGA\Sounoft\Sontest\test.wav")
+        Catch ex As Exception
+            Dim Loadm As String() = My.Settings.VarBut.Split("-SOUNOFT-")
+            MsgBox(ex.Message & vbNewLine & Loadm(1))
+        End Try
+
     End Sub
 End Class
